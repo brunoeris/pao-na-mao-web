d99218290af9a04c209c87b282f09066e7b4c077
Bruno Ferreira <brunoferreira@MacBook-Pro-de-Bruno.local>
2016-11-05 12:59:32 -0300

    initial structure

---
+++ src/app/index.constants.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('paoNaMaoWeb')
+    .constant('URL_BASE_API', 'http://localhost:3000')
+    .constant('URL_BASE', 'http://localhost:4000')
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('paoNaMaoWeb', ['ngAnimate',
+                            'ngCookies',
+                            'ngTouch',
+                            'ngSanitize',
+                            'ngMessages',
+                            'ngAria',
+                            'ngResource',
+                            'ui.router',
+                            'ui.bootstrap',
+                            'toastr']);
+
+})();
